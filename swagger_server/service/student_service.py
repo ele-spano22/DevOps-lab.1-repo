b19569e7bc8eec0bfc62036476dfb0424d4b94f8
Elena Spano <elespano2003@gmail.com>
2026-02-26 16:51:15 +0100

    Addded MongoDB implementation as per 3.1

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -1,41 +1,44 @@
 import os
 import tempfile
 from functools import reduce
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from flask import abort
 
-from tinydb import TinyDB, Query
 
 db_dir_path = tempfile.gettempdir()
 db_file_path = os.path.join(db_dir_path, "students.json")
-student_db = TinyDB(db_file_path)
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongodb:27017/")
+client = MongoClient(MONGO_URI)
+db = client["student_db"]
+collection = db["students"]
+
+def add(student):
+    student_dict = student.to_dict()
+    student_dict.pop("student_id", None)
+
+    result = collection.insert_one(student_dict)
+
+    student_dict["student_id"] = str(result.inserted_id)
+    return student_dict, 201
 
 
-def add(student=None):
-    queries = []
-    query = Query()
-    queries.append(query.first_name == student.first_name)
-    queries.append(query.last_name == student.last_name)
-    query = reduce(lambda a, b: a & b, queries)
-    res = student_db.search(query)
-    if res:
-        return 'already exists', 409
+def get_by_id(student_id):
+    student = collection.find_one({"_id": ObjectId(student_id)})
 
-    doc_id = student_db.insert(student.to_dict())
-    student.student_id = doc_id
-    return student.student_id
-
-
-def get_by_id(student_id=None, subject=None):
-    student = student_db.get(doc_id=int(student_id))
     if not student:
-        return 'not found', 404
-    student['student_id'] = student_id
-    print(student)
+        abort(404, "Student not found")
+
+    student["student_id"] = str(student["_id"])
+    del student["_id"]
+
     return student
 
 
-def delete(student_id=None):
-    student = student_db.get(doc_id=int(student_id))
-    if not student:
-        return 'not found', 404
-    student_db.remove(doc_ids=[int(student_id)])
-    return student_id
+def delete(student_id):
+    result = collection.delete_one({"_id": ObjectId(student_id)})
+
+    if result.deleted_count == 0:
+        abort(404, "Student not found")
+
+    return {}, 200
